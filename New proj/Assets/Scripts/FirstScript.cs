c7bb89749bcaa2cb09fb4db2986ae114b96c0cdf
Yeet 69 <selimkadhi@gmail.com>
2020-07-07 12:45:08 +0100

    hp cp

---
+++ New proj/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FirstScript : MonoBehaviour
+{
+
+    FirstScript script = FindObjectOfType<FirstScript>();
+
+    public void CallName(string name)
+    {
+
+        script.CallName("ada");
+
+        Debug.Log("Hey " + name);
+    }
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New proj/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthManager : MonoBehaviour
+{
+
+    public Slider healthSlider;
+    const float MAXHEALTH = 10f;
+    float health;
+
+// Start is called before the first frame update
+    void Start()
+    {
+
+        health = MAXHEALTH;
+    }
+
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if (health <= 0)
+        {
+            health = 0;
+            Die();
+        }
+        // UPDATE THE SLIDER
+        healthSlider.value = health / MAXHEALTH;
+    }
+
+
+    void Die()
+    {
+        GetComponent<CharacerController2D>().enabled = false;
+        GetComponent<Animator>().SetBool("Dead", true);
+    }
+    public void GetHealth(float amount)
+    {
+        health += amount;
+
+        if (health > MAXHEALTH)
+        {
+            health = MAXHEALTH;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Heal"))
+        {
+            GetHealth(5);
+            Destroy(other.gameObject);
+        }
+        // UPDATE THE SLIDER
+        healthSlider.value = health / MAXHEALTH;
+    }
+
+}
